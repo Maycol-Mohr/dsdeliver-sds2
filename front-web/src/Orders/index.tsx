67de7041adc0a59f55dd737fbef5bf2437a741d9
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-10-09 01:40:18 +0200

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import StepsReader from './StepsHeader';
+import './styles.css'
+
+function Orders() {
+    return (
+        <div className="orders-container">
+            <StepsReader />
+        </div>
+    )
+}
+
+export default Orders;

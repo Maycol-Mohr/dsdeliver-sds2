dba00be8c33b0cb8dbedd035422f658c55500159
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-10-09 12:31:09 +0200

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepsReader from './StepsHeader';
 import './styles.css'
-import { Product } from './types';
+import { OrderLocationdata, Product } from './types';
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
     
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
 
     useEffect(() => {
         fetchProducts()
@@ -21,6 +23,7 @@ function Orders() {
         <div className="orders-container">
             <StepsReader />
             <ProductsList products={products} />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
         </div>
     )
 }

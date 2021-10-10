ab8c1ce676e4b9e3b4eec5156633eb726e869ef7
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-10-10 02:42:35 +0200

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,3 +1,8 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
 import './App.css';
 import Routes from './Routes';
 
@@ -6,7 +11,10 @@ import Routes from './Routes';
 
 function App() {
   return (
+    <>
       <Routes />
+      <ToastContainer />
+    </>
   );
 }
 
